7bfee1bc6a4bb8252f53f4af81e48ab715f977bf
Alexandr Efimchik <efimchiksasha@gmail.com>
2020-12-03 22:16:34 +0300

    Artem task2

---
+++ pascal/03_12_2020/Task2_Arta.pas
@@ -0,0 +1,26 @@
+﻿program task1_efimovich;
+
+var A: array[1..7] of real;
+var sum_otric: real;
+var multi_poloz: real;
+
+begin
+
+multi_poloz := 1;
+
+for var i:=1 to 7 do begin
+  writeln('Enter ' + i + ' num: ');
+  read(A[i]);
+end;
+
+for var i:=1 to 7 do begin
+  if A[i] > 0 then
+    multi_poloz := multi_poloz *A[i] ;
+  if A[i] < 0 then
+    sum_otric := sum_otric  + A[i] ;
+end;
+
+writeln('Сумма отрицательных: ', sum_otric:0:2);
+writeln('Произведение положительных: ', multi_poloz:0:2);
+
+end.
